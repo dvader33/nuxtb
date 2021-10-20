ca0cac2215f04990bed5cee5827430067e254692
Your Name <vhader33.dr@gmail.com>
2021-10-20 12:25:39 -0600

    styles on search page

--- pages/search.vue
+++ pages/search.vue
@@ -1,80 +1,77 @@
 <template>
-    <div>
-        Results For: {{ label }} <br/>
-    <div style="height:800px; width:800px; float:right" ref="map"></div>
-    <div v-if="homes.length > 0">
-        <nuxt-link v-for="home in homes" :key="home.objectID" :to="`/home/${home.objectID}`">
-        <HomeRow  :home="home" @mouseover.native="highlightMarker(home.objectID, true)" @mouseout.native="highlightMarker(home.objectID, false)"/>
-        </nuxt-link>
-    </div>
-    <div v-else>
-        No results found
-    </div>
-    </div>
+<div class="app-search-results-page">
+ <div class="app-search-results">
+   <div class="app-search-results-listing">
+     <h2 class="app-title">Stays in {{ label }}</h2>
+     <nuxt-link v-for="home in homes" :key="home.objectID" :to="`/home/${home.objectID}`">
+       <HomeRow class="app-house" :home="home" @mouseover.native="highlightMarker(home.objectID, true)" @mouseout.native="highlightMarker(home.objectID, false)"/>
+    </nuxt-link> 
+   </div>  
+   <div class="app-search-results-map">
+     <div class="app-map" ref="map"></div>
+   </div>
+ </div> 
+</div>
 </template>
 <script>
 export default {
-    head(){
-        return{
-            title: `Homes around ${this.label}`
-        }
-    },
-    mounted(){
-        this.updateMap()
-    },
-    methods:{
-        highlightMarker(homeId, isHighlighted){
-            document.getElementsByClassName(`home-${homeId}`)[0]?.classList?.toggle('marker-highlight', isHighlighted)
-        },
-        updateMap(){
-            this.$maps.showMap(this.$refs.map, this.lat, this.lng, this.getHomeMarkers())
-
-        },
-        getHomeMarkers(){
-            return this.homes.map((home) => {
-                return{
-                    ...home._geoloc,
-                    pricePerNight: home.pricePerNight,
-                    id: home.objectID
-                }
-            }
-
-            )
-        }
-    },
-   /* watchQuery:['lat'],*/ //opcion para hacerlo con watch no falla en prod puede fallar en dev
-    async beforeRouteUpdate(to, from, next){
-        const data = await this.$dataApi.getHomesByLocation(to.query.lat, to.query.lng)
-        this.homes = data.json.hits,
-        this.label = to.query.label,
-        this.lat = to.query.lat,
-        this.lng = to.query.lng
-        this.updateMap()
-        next()
-    },
-    async asyncData({query, $dataApi}){
-        const data = await $dataApi.getHomesByLocation(query.lat, query.lng)
-        return{
-            homes: data.json.hits,
-            label: query.label,
-            lat: query.lat,
-            lng: query.lng
-        }
+  head(){
+    return {
+      title: `Homes around ${this.label}`
     }
+  },
+  mounted(){
+    this.updateMap()
+  },
+  methods:{
+    highlightMarker(homeId, isHighlighted){
+      document.getElementsByClassName(`home-${homeId}`)[0]?.classList?.toggle('marker-highlight', isHighlighted)
+    },
+    updateMap(){
+      this.$maps.showMap(this.$refs.map, this.lat, this.lng, this.getHomeMarkers())
+    },
+    getHomeMarkers(){
+      if(this.homes.length ==0) return null
+      return this.homes.map((home) => {
+        return {
+          ...home._geoloc,
+          pricePerNight: home.pricePerNight,
+          id: home.objectID,
+        }
+      })
+    }
+  },
+  async beforeRouteUpdate(to, from, next){
+      const data = await this.$dataApi.getHomesByLocation(to.query.lat, to.query.lng)
+      this.homes = data.json.hits
+      this.label = to.query.label
+      this.lat = to.query.lat
+      this.lng = to.query.lng
+      this.updateMap()
+      next()  
+  },
+  async asyncData({ query, $dataApi }){
+    const data = await $dataApi.getHomesByLocation(query.lat, query.lng)
+    return {
+      homes: data.json.hits,
+      label: query.label,
+      lat: query.lat,
+      lng: query.lng,
+    }
+  }
 }
 </script>
 <style>
-.marker{
-    background-color: white;
-    border: 1px solid lightgray;
-    font-weight: bold;
-    border-radius: 20px;
-    padding: 5px 8px;
+.marker {
+  background-color: white;
+  border: 1px solid lightgray;
+  font-weight: bold;
+  border-radius: 20px;
+  padding: 5px 8px;
 }
-
-.marker-highlight{
-    color: white !important;
-    background-color: black;
-    border-color: black;
+.marker-highlight {
+  color: white !important;
+  background-color: black;
+  border-color:black;
 }
 </style>
